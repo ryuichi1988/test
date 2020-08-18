1e6d2a9e3dd5073b4ad9cca7aa7272a114f3899e
testname <r_okuyama@hotmail.com>
2020-08-19 01:59:53 +0900

    的

--- a=1.py
+++
@@ -1,6 +0,0 @@
-from pyautogui import pixel
-with open ("test002248946.txt","w") as file:
-    for x in range (387,499,1):
-        for y in range(354,575,1):
-            file.write("{},{},rgb{}".format(x,y,pixel(x,y))+"\n")
-file.close()
